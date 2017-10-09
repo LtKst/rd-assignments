dc7713bcf246baff1d78a7f802513b8b69a70cb1
Koen Sparreboom <koensparreboom@gmail.com>
2017-10-09 03:14:36 +0200

    Initial commit

---
+++ larson_scanner/larson_scanner.ino
@@ -0,0 +1,34 @@
+int leds[] = {0, 1, 2, 3, 4, 5};
+int ledsLength = 5;	//sizeof is very slow for some reason so array length is hardcoded
+
+void setup() {
+  for (int i = 0; i < ledsLength; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < ledsLength; i++) {
+
+    digitalWrite(leds[i], HIGH);
+    delay(100);
+  }
+
+  for (int i = 0; i < ledsLength; i++) {
+    digitalWrite(leds[i], LOW);
+
+    delay(100);
+  }
+
+  for (int i = ledsLength; i >= 0; i--) {
+    digitalWrite(leds[i], HIGH);
+
+    delay(100);
+  }
+
+  for (int i = ledsLength; i >= 0; i--) {
+    digitalWrite(leds[i], LOW);
+
+    delay(100);
+  }
+}
