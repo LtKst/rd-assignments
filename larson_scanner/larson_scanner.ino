2ba18cd8211f9b5cf63dc1bdd466a6b0fc099952
Koen Sparreboom <koensparreboom@gmail.com>
2017-10-09 03:21:06 +0200

    Improved formatting

--- larson_scanner/larson_scanner.ino
+++ larson_scanner/larson_scanner.ino
@@ -1,32 +1,39 @@
 int leds[] = {0, 1, 2, 3, 4, 5};
 int ledsLength = 5;	//sizeof is very slow for some reason so array length is hardcoded
 
-void setup() {
-  for (int i = 0; i < ledsLength; i++) {
+void setup()
+{
+  for (int i = 0; i < ledsLength; i++)
+  {
     pinMode(leds[i], OUTPUT);
   }
 }
 
-void loop() {
-  for (int i = 0; i < ledsLength; i++) {
-
+void loop()
+{
+  for (int i = 0; i < ledsLength; i++)
+  {
     digitalWrite(leds[i], HIGH);
+    
     delay(100);
   }
 
-  for (int i = 0; i < ledsLength; i++) {
+  for (int i = 0; i < ledsLength; i++)
+  {
     digitalWrite(leds[i], LOW);
 
     delay(100);
   }
 
-  for (int i = ledsLength; i >= 0; i--) {
+  for (int i = ledsLength; i >= 0; i--)
+  {
     digitalWrite(leds[i], HIGH);
 
     delay(100);
   }
 
-  for (int i = ledsLength; i >= 0; i--) {
+  for (int i = ledsLength; i >= 0; i--)
+  {
     digitalWrite(leds[i], LOW);
 
     delay(100);
